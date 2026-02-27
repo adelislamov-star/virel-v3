864ef775e805b688d7b5acdeb96fc6583782c3be
Admin <admin@company.com>
2026-02-27 22:57:27 +0000

    fix: clean next.config force rebuild

--- next.config.js
+++ next.config.js
@@ -8,8 +8,6 @@ const nextConfig = {
   },
   images: {
     formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
       {
         protocol: 'https',
@@ -23,4 +21,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-// cache bust  
